c789e0027a666ce60139b56a06167653d546a655
Sandeep Chandrasekhar <sandeepchandrasekhar@Sandeeps-MBP.home>
2017-03-11 12:49:27 -0500

    overunder game

---
+++ overunder.py
@@ -0,0 +1,60 @@
+# Intro tests for experimenting
+# print('Hello World')
+# print('Guess a Number')
+# input('Guess a Number')
+
+# # Generate a random number
+
+# import random
+
+# number = random.randint(1,10)
+
+# # say whether your guess is too high or too low
+
+# guess = input('Guess a Number')
+# guess = int(guess)
+# print(type(guess))
+# print(type(number))
+
+# # if guess < number:
+# # 	print('Guess is too low.')
+# # 	print('Try again.')
+# # elif guess == number:
+# # 	print('Congratulations, you have guessed the correct number!')
+# # 	print('Your prize will be coming shortly.')
+# # else:
+# # 	print('Guess is too high.')
+# # 	print('Try again.')
+
+# count = 0
+# while (count < 9):
+#    print ('The count is:', count)
+#    count = count + 1
+
+# print ("Good bye!")
+
+import random
+
+number = random.randint(1,100)
+guess = -1
+
+while guess != number:
+	guess = input('Guess a Number')
+	guess = int(guess)
+
+	if guess < number:
+		print('This is too low.')
+		print('Try again.')
+	elif guess == number:
+		print('Congratulations, you win.')
+		print('Click here to get your prize')
+	else:
+		print('THis is too high.')
+		print('Try again.')
+	
+	# print(guess != number)
+
+# count = 0
+# while count < guess:
+# 	print('Number is too low', count)
+# 	count = count + 1
