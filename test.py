175edf9ee8a6e16e0cc997a383cbe69f01464f07
Sandeep Chandrasekhar <sandeep.chandrasekhar@gmail.com>
2017-03-11 13:22:22 -0500

    Random Test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# name = "John"
-guess = input('Guess a Name')
+print('I am a champ!')
+# guess = input('Guess a Name')
 
-print('Hi' + ' ' + guess + ", " + "thanks for checking in with me!")
+# print('Hi' + ' ' + guess + ", " + "thanks for checking in with me!")
